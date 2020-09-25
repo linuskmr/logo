ae26f127a8ecd73011a2f2ef6fdc1aa881ed97f0
linus-k519 <56581587+linus-k519@users.noreply.github.com>
2020-09-25 17:59:38 +0200

    commit

--- llog.go
+++ llog.go
@@ -3,9 +3,11 @@ package llog
 import (
 	"fmt"
 	"github.com/fatih/color"
+	"io"
 	"os"
 	"runtime"
 	"strings"
+	"time"
 )
 
 var (
@@ -15,15 +17,54 @@ var (
 	debugPrefix = color.New(color.FgGreen).Sprint("DEBUG")
 )
 
+const (
+	stdLongYear  = "2006"
+	stdZeroMonth = "01"
+	stdZeroDay   = "02"
+
+	stdHour        = "15"
+	stdZeroMinute  = "04"
+	stdZeroSecond  = "05"
+	stdMillisecond = "000"
+)
+
+const (
+	dateFormat = stdLongYear + "-" + stdZeroMonth + "-" + stdZeroDay
+	timeFormat = stdHour + ":" + stdZeroMinute + ":" + stdZeroSecond
+)
+
+const (
+	Fdate = 1 << iota
+	Ftime
+)
+
+var (
+	Output io.Writer = os.Stdout
+	Flags            = Fdate | Ftime
+)
+
 func shortFilename(filename string) string {
 	filePath := strings.Split(filename, "/")
 	return filePath[len(filePath)-1]
 }
 
+func currentTime() string {
+	if Flags&Fdate != 0 && Flags&Ftime != 0 {
+		return time.Now().Format(dateFormat + " " + timeFormat)
+	}
+	if Flags&Fdate != 0 {
+		return time.Now().Format(dateFormat)
+	}
+	if Flags&Ftime != 0 {
+		return time.Now().Format(timeFormat)
+	}
+	return ""
+}
+
 func header(mode string) string {
 	caller, file, line, _ := runtime.Caller(2)
 	functionName := runtime.FuncForPC(caller).Name()
-	return fmt.Sprintf(`%s %s:%d %s:`, mode, shortFilename(file), line, shortFilename(functionName))
+	return fmt.Sprintf(`%s %s %s:%d %s:`, mode, currentTime(), shortFilename(file), line, shortFilename(functionName))
 }
 
 func spaceJoiner(v ...interface{}) string {
@@ -35,17 +76,17 @@ func spaceJoiner(v ...interface{}) string {
 }
 
 func Info(v ...interface{}) {
-	fmt.Fprintln(os.Stdout, header(infoPrefix), spaceJoiner(v...))
+	fmt.Fprintln(Output, header(infoPrefix), spaceJoiner(v...))
 }
 
 func Error(v ...interface{}) {
-	fmt.Fprintln(os.Stdout, header(errorPrefix), spaceJoiner(v...))
+	fmt.Fprintln(Output, header(errorPrefix), spaceJoiner(v...))
 }
 
 func Debug(v ...interface{}) {
-	fmt.Fprintln(os.Stdout, header(debugPrefix), spaceJoiner(v...))
+	fmt.Fprintln(Output, header(debugPrefix), spaceJoiner(v...))
 }
 
 func Print(v ...interface{}) {
-	fmt.Fprintln(os.Stdout, header(printPrefix), spaceJoiner(v...))
+	fmt.Fprintln(Output, header(printPrefix), spaceJoiner(v...))
 }
--- llog_test.go
+++ llog_test.go
@@ -1,21 +1,60 @@
 package llog
 
 import (
+	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
-func TestMyLogger_Info(t *testing.T) {
-	Info("Info", "Test")
-}
+func TestInfo(t *testing.T) {
+	stringBuilder := strings.Builder{}
+	Output = &stringBuilder
+	const (
+		mode     = "INFO"
+		dateTime = "\\d\\d\\d\\d-\\d\\d-\\d\\d\\s\\d\\d:\\d\\d:\\d\\d"
+		filename = "llog_test.go:18"
+		method   = "llog.TestInfo"
+	)
+	Info(mode)
+	assert.Regexp(t, "("+mode+")\\s+"+dateTime+"\\s("+filename+")\\s("+method+": "+mode+")", stringBuilder.String())
 
-func TestPrint(t *testing.T) {
-	Print("Print")
-}
-
-func TestError(t *testing.T) {
-	Error("Error")
 }
 
 func TestDebug(t *testing.T) {
-	Debug("Debug")
+	stringBuilder := strings.Builder{}
+	Output = &stringBuilder
+	const (
+		mode     = "DEBUG"
+		dateTime = "\\d\\d\\d\\d-\\d\\d-\\d\\d\\s\\d\\d:\\d\\d:\\d\\d"
+		filename = "llog_test.go:32"
+		method   = "llog.TestDebug"
+	)
+	Debug(mode)
+	assert.Regexp(t, "("+mode+")\\s+"+dateTime+"\\s("+filename+")\\s("+method+": "+mode+")", stringBuilder.String())
+}
+
+func TestError(t *testing.T) {
+	stringBuilder := strings.Builder{}
+	Output = &stringBuilder
+	const (
+		mode     = "ERROR"
+		dateTime = "\\d\\d\\d\\d-\\d\\d-\\d\\d\\s\\d\\d:\\d\\d:\\d\\d"
+		filename = "llog_test.go:45"
+		method   = "llog.TestError"
+	)
+	Error(mode)
+	assert.Regexp(t, "("+mode+")\\s+"+dateTime+"\\s("+filename+")\\s("+method+": "+mode+")", stringBuilder.String())
+}
+
+func TestPrint(t *testing.T) {
+	stringBuilder := strings.Builder{}
+	Output = &stringBuilder
+	const (
+		mode     = "PRINT"
+		dateTime = "\\d\\d\\d\\d-\\d\\d-\\d\\d\\s\\d\\d:\\d\\d:\\d\\d"
+		filename = "llog_test.go:58"
+		method   = "llog.TestPrint"
+	)
+	Print(mode)
+	assert.Regexp(t, "("+mode+")\\s+"+dateTime+"\\s("+filename+")\\s("+method+": "+mode+")", stringBuilder.String())
 }
