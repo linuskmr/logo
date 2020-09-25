53a45652f7b2141441c8c997fb2e1071bff686a4
linus-k519 <56581587+linus-k519@users.noreply.github.com>
2020-09-25 13:20:41 +0200

    commit

--- llog.go
+++ llog.go
@@ -1,32 +1,46 @@
 package llog
 
 import (
+	"fmt"
 	"github.com/fatih/color"
 	"log"
 	"os"
+	"runtime"
+	"strings"
 )
 
 const flags = log.LstdFlags | log.Lshortfile
 
 var (
-	infoLog  = log.New(os.Stdout, color.New(color.FgBlue).Sprint("INFO  "), flags)
-	printLog = log.New(os.Stdout, "PRINT ", flags)
-	errorLog = log.New(os.Stdout, color.New(color.FgRed).Sprint("ERROR "), flags)
-	debugLog = log.New(os.Stdout, color.New(color.FgGreen).Sprint("DEBUG "), flags)
+	infoPrefix  = color.New(color.FgBlue).Sprint("INFO")
+	printPrefix = "PRINT"
+	errorPrefix = color.New(color.FgRed).Sprint("ERROR ")
+	debugPrefix = color.New(color.FgGreen).Sprint("DEBUG ")
 )
 
+func shortFilename(filename string) string {
+	filePath := strings.Split(filename, "/")
+	return filePath[len(filePath)-1]
+}
+
+func header(mode string) string {
+	caller, file, line, _ := runtime.Caller(2)
+	functionName := runtime.FuncForPC(caller).Name()
+	return fmt.Sprintf("%-5s %s:%d %s:", mode, shortFilename(file), line, shortFilename(functionName))
+}
+
 func Info(v ...interface{}) {
-	infoLog.Println(v...)
+	fmt.Fprintln(os.Stdout, header(infoPrefix), v)
 }
 
 func Error(v ...interface{}) {
-	errorLog.Println(v...)
+	fmt.Fprintln(os.Stdout, header(errorPrefix), v)
 }
 
 func Debug(v ...interface{}) {
-	debugLog.Println(v...)
+	fmt.Fprintln(os.Stdout, header(debugPrefix), v)
 }
 
 func Print(v ...interface{}) {
-	printLog.Println(v...)
+	fmt.Fprintln(os.Stdout, header(printPrefix), v)
 }
